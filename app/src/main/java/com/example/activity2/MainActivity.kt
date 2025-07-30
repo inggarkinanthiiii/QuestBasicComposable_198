b8ca5fcfe21831b46f9a0bcc17fcdc461f7b5b28
Inggar Kinanthi <inggarkinanthi@example.com>
2025-07-30 14:10:28 +0700

    menampilkan UI menggunakan Jetpack

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -4,3 +4,5 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+
