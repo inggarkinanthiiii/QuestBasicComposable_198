8528a56bdc6b7c2d6b516b761941f3592d1c203f
Inggar Kinanthi <inggarkinanthi@example.com>
2025-07-30 14:15:55 +0700

    create

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -10,4 +10,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.activity2.ui.theme.Activity2Theme
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
 
