9e0e2772308cbb5811cd92d1e4edeb1de1670c81
Inggar Kinanthi <inggarkinanthi@example.com>
2025-07-30 14:15:21 +0700

    perubahan

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -8,4 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.activity2.ui.theme.Activity2Theme
+
 
